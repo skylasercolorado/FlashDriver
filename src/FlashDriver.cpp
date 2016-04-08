09cf42263dc9124820a427b73eabe106b0e14426
Joe Rohd <joerohd@gmail.com>
2016-04-07 21:08:27 -0700

    refactoring program flash to add other error checks, part 10

--- src/FlashDriver.cpp
+++ src/FlashDriver.cpp
@@ -16,19 +16,7 @@ FlashResult FlashDriver::Program(ioAddress address, ioData data)
     while(!((status = io_.IoRead(FlashRegisters::Status)) & FlashStatus::Ready));
 
     if(status & ~FlashStatus::Ready)
-    {
         return processError(status, io_);
-//        io_.IoWrite(FlashRegisters::Control, FlashCommands::Reset);
-//
-//        if(status & FlashStatus::VppError)
-//            return FlashResult::Vpp_Error;
-//        else if(status & FlashStatus::ProgramError)
-//            return FlashResult::Program_Error;
-//        else if(status & FlashStatus::ProtectedBlockError)
-//            return FlashResult::ProtectedBlock_Error;
-//        else
-//            return FlashResult::Unknown_Error;
-    }
 
     return io_.IoRead(address) == data ? FlashResult::Success : FlashResult::ReadBack_Error;
 }
